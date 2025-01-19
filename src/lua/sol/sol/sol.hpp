0cdd62f08855c70c5257bc84d3d455e818cbb1e0
hk <hossam.karim@gmail.com>
2025-01-19 15:18:00 +0200

    macos arm changes

--- src/lua/sol/sol/sol.hpp
+++ src/lua/sol/sol/sol.hpp
@@ -6058,7 +6058,7 @@ namespace sol {
 			*this = nullopt;
 			//this->construct(std::forward<Args>(args)...);
 			new (static_cast<void*>(this)) optional(std::in_place, std::forward<Args>(args)...);
-			//return **this;
+			return **this;
 		}
 
 		/// Swaps this optional with the other.
